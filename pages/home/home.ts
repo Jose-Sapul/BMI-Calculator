a356f8d673a5805924b063541a3ad121ea09caf4
Jose-Sapul <jose.sapul090290@my.lsgh.edu.ph>
2021-04-19 09:17:08 +0800

    1 file modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
 })
 export class HomePage {
   H: number;
